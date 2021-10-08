0495a042519c295ab4986f176bc646ad89f050ba
dangduc123 <91090157+dangduc123@users.noreply.github.com>
2021-10-08 14:14:48 +0700

    Update nhapXuatMaTran.cpp

--- nhapXuatMaTran.cpp
+++ nhapXuatMaTran.cpp
@@ -77,14 +77,59 @@ int inSNT(int a[100][100],int n,int m)
 	}
 	return dem;
 }
+
+//tinh tong tren mot dong
+float tongDong(int a[100][100],int n,int m,int x)
+{
+	int tong = 0;
+	for(int j = 0;j<m;j++)
+	{
+		tong =tong+a[x][j];
+	}
+	return tong;
+}
+
+
+float tongCot(int a[100][100],int n,int m,int x)
+{
+	int tong = 0;
+	for(int i = 0;i<n;i++)
+	{
+		tong = tong + a[i][x];
+	}
+	return tong;
+}
+
+int max_Matran(int a[100][100],int n,int m)
+{
+	int max = a[0][0];
+	for(int i = 0;i<n;i++)
+	{
+		for(int j = 0;j<m;j++)
+		{
+			if(max < a[i][j])
+			{
+				max = a[i][j];
+			}
+		}
+	}
+	return max;
+}	
 			
 
 int main()
 {
 	int a[100][100];
-	int n,m;
+	int n,m,x;
 	nhapMaTran(a,n,m);
 	xuatMaTran(a,n,m);
 	printf("\nSo phan tu chan la:%d",pTChan(a,n,m));
 	printf("\nCac so nguyen to la:%d",inSNT(a,n,m));
+	printf("\nNhap dong can tinh:");
+	scanf("%d",&x);
+	printf("\nTong cua dong %d trong ma tran la:%f",x,tongDong(a,n,m,x));
+	printf("\nNhap cot can tinh:");
+	scanf("%d",&x);
+	printf("\nTong cua cot %d trong ma tran la:%f",x,tongCot(a,n,m,x));
+	printf("\nSo lon nhat trong ma tran la:%d",max_Matran(a,n,m));
 }
